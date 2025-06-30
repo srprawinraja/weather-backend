d9e06fc8afca17548122ef821c0763527c791893
SR PRAWIN RAJA <127335131+srprawinraja@users.noreply.github.com>
2025-06-30 13:03:35 +0530

    added comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from services import  getWeatherService
 
-app = FastAPI()
+app = FastAPI()  # get Fast api instance
 
-@app.get("/weather/")
+@app.get("/weather/") # sue fast api instance to link the route with the handler
 def getWeather(city_name:str):
-    return getWeatherService(city_name)
+    return getWeatherService(city_name) # return response
